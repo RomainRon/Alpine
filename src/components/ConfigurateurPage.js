5d581313d798073fc9759741a4b96d692257fe60
RomainRon <rondepierreromain@gmail.com>
2024-06-11 16:45:25 +0200

    test

--- src/components/ConfigurateurPage.js
+++ src/components/ConfigurateurPage.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import Header from './Header';
 import { useSelector } from 'react-redux';
+import BlancGlacier from '../images/configurateur/couleurs/selection/blanc.jpg';
+import BleuAlpine from '../images/configurateur/couleurs/selection/bleu.jpg';
+import NoirProfond from '../images/configurateur/couleurs/selection/noir.jpg';
+import JanteStandard from '../images/configurateur/jantes/selection/jante-standard.jpg';
+import JanteSerac from '../images/configurateur/jantes/selection/jante-serac.jpg';
+import JanteLegend from '../images/configurateur/jantes/selection/jante-legende.jpg';
 
 const Configurateur = () => {
   const pure = useSelector(state => state.configurateur.pure);
